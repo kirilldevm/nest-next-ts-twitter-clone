6b2d78e04a9e9814fdc21a2972bd711203041aac
kirilldev <kirill.makliuk.dev@gmail.com>
2026-03-06 14:39:27 +0200

    feat: frontend config & app hosting deploy

---
+++ client/src/config/pages.config.ts
@@ -0,0 +1,13 @@
+export const PAGES = {
+  HOME: '/',
+
+  //Auth
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FORGOT_PASSWORD: '/reset-password',
+  NEW_PASSWORD: '/new-password',
+  ERROR: '/error',
+  VERIFY: '/verify',
+
+  //Protected
+};
